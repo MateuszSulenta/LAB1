ed5ee69cf757b4a7b76788108566d0ac21cd141f
MateuszSulenta <100996793+MateuszSulenta@users.noreply.github.com>
2022-03-19 22:30:35 +0100

    Użycie funkcji print

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,8 +2,23 @@ package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
 
-    public static voiid main(String[] args) {
+    public static void main(String[] args) {
 	System.out.println("Hello Java World \n");
+        System.out.println("Witam");
+        System.out.println("w");
+        System.out.println("moim");
+        System.out.println("kursie");
+
+        System.out.println("--------------\n");
+
+        System.out.println("Tutaj dowiesz się\n");
+        System.out.println("W jaki sposób\n");
+        System.out.println("nauczyć się\n");
+        System.out.println("programować :)\n");
+
+
+
+
     }
 }
 
