d9b63d84f3f822506a8173553a2ff4b0b726242e
MateuszSulenta <100996793+MateuszSulenta@users.noreply.github.com>
2022-03-20 10:20:19 +0100

    Wypisanie tabeli logarytmów

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,11 +1,31 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import static java.lang.Math.log;
+
 public class Main {
+    static final String FORMAT = "%-10s%-10s%-10s%n";
 
     public static void main(String[] args) {
-        System.out.printf("alfa\tsin(alfa)\n");
-        for (int i = 0; i < 370; i += 10)
-            System.out.printf("%d\t%f\t\n", i, Math.sin(i / 360.0 * 2 * Math.PI));
+        System.out.printf(FORMAT, "arg", "log2", "pow");
+        for (int arg = 1, pow = 1; arg < 5; arg *= 2, pow++) {
+            print(arg, log2(arg), sumPowers(pow));
+        }
+    }
+
+    static int log2(int arg) {
+        return (int) (log(arg) / log(2));
+    }
+
+    static int sumPowers(int arg) {
+        var sum = 0;
+        for (int i = 0; i < arg; i++) {
+            sum += Math.pow(2, i);
+        }
+        return sum;
+    }
+
+    static void print(int arg, int log2, int pow) {
+        System.out.printf(FORMAT, arg, log2, pow);
     }
 }
 
