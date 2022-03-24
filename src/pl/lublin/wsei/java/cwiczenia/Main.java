c0d61cb63354ca4a3d718dd7a1984452c0bcf83a
MateuszSulenta <100996793+MateuszSulenta@users.noreply.github.com>
2022-03-24 20:31:45 +0100

    Zadanie 10. a-d

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,20 +1,33 @@
 package pl.lublin.wsei.java.cwiczenia;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int num1 = 0, num2=0;
-        do {
-
-            System.out.print("Podaj liczbe, które mam dodać: ");
-            num1 = input.nextInt();
-            num2 = input.nextInt();
-            if ((num1 == 0) || (num2 == 0)) break;
-
-            System.out.printf("Wynik dodawania %d +%d = %d%n", num1, num2, num1 + num2);
+        Scanner skaner = new Scanner(System.in);
+        System.out.println("Wprowadź liczbę");
 
 
+        int myNumber = Integer.parseInt(skaner.nextLine());
 
-        } while(true);
+        System.out.println("DEC = " +
+                myNumber +
+                ", BIN = " +
+                leftPad(Integer.toBinaryString(myNumber),'0',8) +
+                ", " +
+                "HEX = " +
+                "%" +
+                leftPad(Integer.toHexString(myNumber).toUpperCase(),'0',4));
+
+
+
     }
+    public static String leftPad(String aText, char aChar, int aWidth) {
+        String res = aText;
+        for (int i = 0; i <aWidth - aText.length(); i++ )
+            res = aChar + res;
+        return res;
+
+
+    }
+
 }
