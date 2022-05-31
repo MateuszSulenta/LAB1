06d3cf6cfe033270930c62963bfae536aea14efe
MateuszSulenta <100996793+MateuszSulenta@users.noreply.github.com>
2022-05-31 17:54:16 +0200

    Zdefiniowanie capitalize jako public static

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -4,33 +4,36 @@ import pl.lublin.wsei.java.mylib.Account;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner skaner = new Scanner(System.in);
-        System.out.println("Wprowadź liczbę");
+        public static void main(String[] args) {
+            Scanner skaner = new Scanner(System.in);
+            System.out.println("Wprowadź liczbę");
 
 
-        int myNumber = Integer.parseInt(skaner.nextLine());
 
-        System.out.println("DEC = " +
-                myNumber +
-                ", BIN = " +
-                leftPad(Integer.toBinaryString(myNumber), '0', 8) +
-                ", " +
-                "HEX = " +
-                "%" +
-                leftPad(Integer.toHexString(myNumber).toUpperCase(), '0', 4));
+            int myNumber = Integer.parseInt(skaner.nextLine());
 
-        Account acc = new Account();
-        acc.setName("piotr Gołąbek");
-        System.out.println(acc.getName());
+            System.out.println("DEC = " +
+                    myNumber +
+                    ", BIN = " +
+                    leftPad(Integer.toBinaryString(myNumber), '0', 8) +
+                    ", " +
+                    "HEX = " +
+                    "%" +
+                    leftPad(Integer.toHexString(myNumber).toUpperCase(), '0', 4));
 
+            Account acc = new Account();
+            acc.setName("piotr Gołąbek");
+            System.out.println(acc.getName());
 
-    }
+        System.out.printf("%s%n", Account.capitalize("stanisŁaw maruSARz"));
+
+        }
 
     private static String leftPad(String aText, char aChar, int aWidth) {
         String res = aText;
         for (int i = 0; i < aWidth - aText.length(); i++)
             res = aChar + res;
         return res;
+
     }
 }
--- src/pl/lublin/wsei/java/mylib/Account.java
+++ src/pl/lublin/wsei/java/mylib/Account.java
@@ -1,17 +1,24 @@
 package pl.lublin.wsei.java.mylib;
+
 import org.apache.commons.lang3.StringUtils;
+
 public class Account {
     String name;
 
-  public void setName(String name) {
-      String[] tokens = StringUtils.split(name);
-      for(int i = 0; i < tokens.length; i++)
-          tokens[i] = StringUtils.capitalize(tokens[i]);
-      this.name = StringUtils.join(tokens, ' ');
+    public static String capitalize(String aStr) {
+        String[] tokens = StringUtils.split(aStr);
+        for (int i = 0; i < tokens.length; i++)
+            tokens[i] = StringUtils.capitalize(tokens[i]);
+        return StringUtils.join(tokens, ' ');
+    }
 
-  }
-    public String getName(){
-      return name;
+    public void setName(String name) {
+        this.name = capitalize(name);
+
+    }
+
+    public String getName() {
+        return name;
     }
 
 }
